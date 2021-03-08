763f667aabc7c6c42f14ee9424a84b5d7aef7588
libbychaps <s1739729@ed.ac.uk>
2021-03-08 18:04:24 +0000

    changed variable "NoConFirstYear" to "NumConFirstYear" to remove confusion

--- Rconsorts.R
+++ Rconsorts.R
@@ -12,7 +12,7 @@ View(consorts)
     mutate(ConFirstYear = case_when(ConAge == 0 ~ "1",   #gives 1 to all individuals that consorted in their first year
                                    ConAge >= 1 ~ "0"))  #gives 0 to all individuals that did not hold consort in first year
 
-  NoConFirstYear<- length(consorts$ConFirstYear[consorts$ConFirstYear == 1]) #2260 obs of first year consorts
+  NumConFirstYear<- length(consorts$ConFirstYear[consorts$ConFirstYear == 1]) #2260 obs of first year consorts
   length(consorts$Date)  #11273 total consort obs
   PropCon = (NoConFirstYear/11273)  #proportion of obs that were first year males --> 0.200479
   PropCon

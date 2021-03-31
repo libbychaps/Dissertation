babf3c4872a9397fb06d43edc2fd5ecc29f83c61
libbychaps <s1739729@ed.ac.uk>
2021-03-31 16:20:38 +0100

    further attempting to plot interaction

--- Rsheep.R
+++ Rsheep.R
@@ -1052,13 +1052,7 @@ str(sheep)
     #plot in base R
     plot(Weight~SurvivedFirstYear,data=sheep,col=as.factor(PopType))
     
-    PopType1 <- filter(sheep,PopType == 1)
-    PopType0 <- filter(sheep,PopType == 0)
     
-    oop<- ggplot(sheep,aes=(x=Weight,y=SurvivedFirstYear),color=PopType))+
-      geom_point()+
-      geom_smooth(method="NULL",data=PopType1)+
-      geom_smooth(method="NULL",data=PopType0)
     
     
     
